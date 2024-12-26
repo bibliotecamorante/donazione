9b1065b2f93405c353a896c903fbf1657a332d3f
bibliotecamorante <bibliotecamorante@gmail.com>
2024-12-26 14:59:09 +0100

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,11 @@
-// config.js
 const config = {
   emailjs: {
-    serviceId: window?.__env__?.EMAILJS_SERVICE_ID || 'service_yepi8uu',
-    templateId: window?.__env__?.EMAILJS_TEMPLATE_ID || 'template_s1jmd7r',
-    publicKey: window?.__env__?.EMAILJS_PUBLIC_KEY || 'z6OeY42qSulJ-8EcS'
+    serviceId: window?.env?.EMAILJS_SERVICE_ID || 'service_yepi8uu',
+    templateId: window?.env?.EMAILJS_TEMPLATE_ID || 'template_s1jmd7r',
+    publicKey: window?.env?.EMAILJS_PUBLIC_KEY || 'z6OeY42qSulJ-8EcS'
   },
   googleBooks: {
-    apiKey: window?.__env__?.GOOGLE_BOOKS_API_KEY || 'AIzaSyCHJzsPtp8CfKeUag-vvyTxobwR_DmYink',
+    apiKey: window?.env?.GOOGLE_BOOKS_API_KEY || 'AIzaSyCHJzsPtp8CfKeUag-vvyTxobwR_DmYink',
     apiOptions: {
       mode: 'cors',
       headers: {
@@ -15,11 +14,14 @@ const config = {
     }
   },
   library: {
-    email: window?.__env__?.LIBRARY_EMAIL || 'bibliotecamorante@gmail.com'
+    email: window?.env?.LIBRARY_EMAIL || 'bibliotecamorante@gmail.com'
+  },
+  api: {
+    rateLimiter: '/api/rateLimiter'
   }
 };
 
-// Aggiungi funzione per gestire le chiamate API con CORS
+// Function to handle API requests with CORS
 config.fetchWithCORS = async (url, options = {}) => {
   const defaultOptions = {
     mode: 'cors',
